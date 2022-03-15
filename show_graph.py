5e5fd99ec72a0ed4d94534e5365a1a59bf65847c
Adrian Lopez <adrianlzt@gmail.com>
2022-03-15 12:31:45 +0100

    No visualizar alarms
    Si intentamos pintar el grafo con las alarmas, no conseguimos ver nada
    por la cantidad de nodos.

--- show_graph.py
+++ show_graph.py
@@ -10,15 +10,18 @@ with open('graph.cyjs') as f:
 
 G = nx.cytoscape_graph(data)
 
+# Remove the alarm nodes, to improve visualization.
+# Create a copy of the graph, to avoid modifying while iterating
+for node in G.copy().nodes():
+    # Drop alarm nodes, its ids are only numeric
+    try:
+        int(node)
+        G.remove_node(node)
+    except ValueError:
+        pass
+
 pos = nx.spring_layout(G, k=0.5, iterations=50)
 pos_nodes = nudge(pos, 0, 0.05)
 nx.draw_networkx(G, pos=pos, with_labels=False)
 nx.draw_networkx_labels(G, pos=pos_nodes)
 plt.show()
-
-#nx.draw_networkx(G)
-#
-#ax = plt.gca()
-#ax.margins(0.20)
-#plt.axis("off")
-#plt.show()
